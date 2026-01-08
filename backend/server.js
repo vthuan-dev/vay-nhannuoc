8e4178fa508cb7066fdff5fd2e02de9a137a871a
Nguyễn Văn Thuần <165972755+vthuan-dev@users.noreply.github.com>
2026-01-09 00:57:12 +0700

    Fix SMTP config for Render

--- backend/server.js
+++ backend/server.js
@@ -68,10 +68,15 @@ const storage = multer.diskStorage({
 const upload = multer({ storage: storage }).any();
 
 const transporter = nodemailer.createTransport({
-    service: 'gmail',
+    host: 'smtp.gmail.com',
+    port: 587,
+    secure: false,
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
+    },
+    tls: {
+        rejectUnauthorized: false
     }
 });
 
