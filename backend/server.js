30852da53ffdd96d9c1822b3bccb669d6a54067f
Nguyễn Văn Thuần <165972755+vthuan-dev@users.noreply.github.com>
2026-01-10 10:08:03 +0700

    S

--- backend/server.js
+++ backend/server.js
@@ -76,15 +76,10 @@ const storage = multer.diskStorage({
 const upload = multer({ storage: storage }).any();
 
 const transporter = nodemailer.createTransport({
-    host: 'smtp.gmail.com',
-    port: 587,
-    secure: false,
+    service: 'gmail',
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
-    },
-    tls: {
-        rejectUnauthorized: false
     }
 });
 
